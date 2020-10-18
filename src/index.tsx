dfee09cb8b944e26a28329099ac05c88d84b4bbd
sdesingh <sdesingh@pm.me>
2020-10-17 22:03:05 -0400

    fixed dependency for primereact and added arya-orange theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "primereact/resources/themes/arya-orange/theme.css";
+import "primereact/resources/primereact.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
