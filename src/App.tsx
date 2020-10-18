718cb6f0d7c3560e6606261d5966be22ee59e0f7
sdesingh <sdesingh@pm.me>
2020-10-17 20:01:53 -0400

    fix button tag

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="title">mujik</div>
       <div className="sub-title">Currently in development. Now continuously delivering features!</div>
-      <button style={{marginTop: "10px"}}>Sign In
+      <button style={{marginTop: "10px"}}>Sign In</button>
     </div>
   );
 }
