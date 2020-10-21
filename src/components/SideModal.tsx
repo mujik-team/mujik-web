024f1bb7ca874c544c882e8fe607d79571196c0c
sdesingh <sdesingh@pm.me>
2020-10-21 19:20:43 -0400

    converted SideModal to be a stylized-component

--- src/components/SideModal.tsx
+++ src/components/SideModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 interface Props {
   isActive: boolean;
@@ -6,18 +7,52 @@ interface Props {
   children?: React.ReactNode;
 }
 
+const Modal = styled.div`
+  transform: translateX(100%);
+  position: fixed;
+  background-color: var(--main-bg-color);
+  right: 0;
+  width: 450px;
+  height: 100%;
+  transition: 0.3s ease-in-out all;
+  box-shadow: -5px 0px 30px 5px #00000060;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+  opacity: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  position: fixed;
+  transition: 0.3s ease-in-out all;
+
+  &.active {
+    opacity: 1;
+    z-index: 1;
+    background-color: rgba(0, 0, 0, 0.651);
+  }
+
+  &.active > ${Modal} {
+    transform: translateX(0%);
+  }
+`;
+
 function SideModal(props: Props) {
   return (
-    <div
+    <Container
       onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         props.toggle();
       }}
       className={`side-modal-container ${props.isActive ? "active" : ""}`}
     >
-      <div onClick={(e: any) => e.stopPropagation()} className="side-modal">
+      <Modal onClick={(e: any) => e.stopPropagation()} className="side-modal">
         {props.children}
-      </div>
-    </div>
+      </Modal>
+    </Container>
   );
 }
 
