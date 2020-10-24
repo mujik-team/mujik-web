145754355a58202a776b3b32938dd6b88144bc80
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2020-10-24 19:36:34 +0530

    added register modal

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,11 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../App";
 import { Button } from "primereact/button";
+import SideModal from "../../components/SideModal";
+import RegisterModal from "./components/RegisterModal";
 
 function WelcomePage() {
   const authContext = useContext(AuthContext);
+
+  const toggleModal = () => {
+    setRegisterModal(!showRegisterModal);
+  };
+
+  const [showRegisterModal, setRegisterModal] = useState(false);
+
   return (
     <div>
+      <SideModal isActive={showRegisterModal} toggle={toggleModal}>
+        <RegisterModal />
+      </SideModal>
       <h1>mujik</h1>
       <h3>This site is currently in development.</h3>
       <Button
@@ -15,6 +27,7 @@ function WelcomePage() {
       >
         Login
       </Button>
+      <Button onClick={() => toggleModal()}>Register</Button>
     </div>
   );
 }
---
+++ src/pages/WelcomePage/components/RegisterModal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RegisterModal() {
+  
+  return (
+    <div>
+      This is the registeration modal.
+    </div>
+  );
+}
+
+export default RegisterModal;
