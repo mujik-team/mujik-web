ea3dad1e3ff3b82abd3be51afbc9b6d809b9aae2
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2020-10-26 17:04:04 +0530

    added input boxes for registeration

--- src/pages/WelcomePage/components/RegisterModal.tsx
+++ src/pages/WelcomePage/components/RegisterModal.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import styles from "./RegisterModal.module.css";
 
 function RegisterModal() {
   
   return (
     <div>
-      This is the registeration modal.
+      <div className={styles.registerCardsContainer}>
+        This is the registeration modal.
+        <div className={styles.registerItems}>
+          <div>
+            <span>Username</span>
+            <div className={styles.inputCard}></div>
+          </div>
+          <div>
+            <span>Email</span>
+            <div className={styles.inputCard}></div>
+          </div>
+          <div>
+            <span>Password</span>
+            <div className={styles.inputCard}></div>
+          </div>
+          <div>
+            <span>Confirm Password</span>
+            <div className={styles.inputCard}></div>
+          </div>
+          <div>
+            <span>What do you want users to know about you?</span>
+            <div className={styles.longInputCard}>Bio input box</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
