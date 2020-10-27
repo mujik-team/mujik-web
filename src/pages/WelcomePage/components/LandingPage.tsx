889097c6b77ed7412442d8d7067a600708757c8c
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2020-10-27 18:08:30 +0530

    fixed landing screen css

--- src/pages/WelcomePage/components/LandingPage.tsx
+++ src/pages/WelcomePage/components/LandingPage.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import Button from "../../../components/Button";
 import styles from "./LandingPage.module.css";
 
 function LandingPage(props: any) {
 
   return (
     <div>
-      <h1> mujik </h1> 
+      <div className={styles.title}>mujik</div> 
       <div className={styles.landingContainer}>
         Welcome to mujik, a place to find a mixtape for any moment, feeling or vibe.
       </div>
-      <div>
-        Already have an account? <div className={styles.loginButton}
-        onClick={() => props.setLogin(true)}> Login </div>
+      <div className={styles.registerButton}>
+        Register
+      </div>
+      <div className={styles.login}>
+        Already have an account? <Button className={styles.loginButton}
+        onClick={() => props.setLogin(true)}> Login </Button>
       </div>
     </div>
   );
