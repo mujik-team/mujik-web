4ec5657e3207fcee20bd8554f297fcb2023a5ed3
sdesingh <sdesingh@pm.me>
2020-12-15 00:07:18 -0500

    user submission cards

--- src/pages/TournamentDetailsPage/components/State.Ended/TournamentResults.tsx
+++ src/pages/TournamentDetailsPage/components/State.Ended/TournamentResults.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import { AuthContext } from "../../../../App";
 import AvatarImage from "../../../../components/AvatarImage";
 import TextInput from "../../../../components/Input/TextInput";
 import MixtapeBrowser from "../../../../components/MixtapeBrowser/MixtapeBrowser";
@@ -8,7 +9,9 @@ import { GetSeveralMixtapes } from "../../../../services/mixtapeService";
 
 function TournamentResults(props: Props) {
   const [mixtapes, setMixtapes] = useState([] as any[]);
+  const [showRedeemReward, setShowRedeemReward] = useState(false);
   const history = useHistory();
+  const authContext = useContext(AuthContext);
 
   useEffect(() => {
     if (props.tournament) {
@@ -30,9 +33,84 @@ function TournamentResults(props: Props) {
         })
         .sort((t1, t2) => t2.NumVotes - t1.NumVotes);
 
-      return submissions.slice(0, 3);
-    } else return [];
+      const data = submissions.slice(0, 3);
+
+      const elements = data.map((s, i) => {
+        return (
+          <div>
+            <WinnerCard
+              strokeColor={cardStrokes[i]}
+              key={i}
+              onClick={() => history.push(`/mixtape/${s.MixtapeId}`)}
+            >
+              <AvatarImage username={s.CreatedBy} size={100} />
+              <div className="details">
+                <div className="username">{s.CreatedBy}</div>
+                <div className="num-votes">{`Received ${s.NumVotes} Votes`}</div>
+              </div>
+
+              <img
+                src={`/images/medals/medal-${i + 1}.svg`}
+                height={80}
+                alt={`${i} Place`}
+                className="medal"
+              />
+            </WinnerCard>
+          </div>
+        );
+      });
+
+      return { data, elements };
+    } else return { data: [], elements: [] };
   }, [props.tournament]);
+
+  const yourSubmission = useMemo(() => {
+    if (props.tournament) {
+      const { username } = authContext.currentUser;
+      const userSubmission = props.tournament.Submissions[username];
+      // User has entered tournament.
+      if (userSubmission) {
+        // Check if user is a winner.
+        const winnerIndex = winners.data.findIndex(
+          (w) => w.CreatedBy === username
+        );
+        if (winnerIndex !== -1) {
+          return (
+            <div className="user-submission-container">
+              <div className="title">Your Submission</div>
+              <div className="desc">
+                Congratulations! You won! Click below to redeem your rewards!
+              </div>
+              {winners.elements[winnerIndex]}
+            </div>
+          );
+        } else {
+          return (
+            <div className="user-submission-container">
+              <div className="title">Your Submission</div>
+              <div className="desc">
+                Thank you for entering this tournament. Click below to redeem
+                your rewards!
+              </div>
+              <WinnerCard
+                onClick={() =>
+                  history.push(`/mixtape/${userSubmission.MixtapeId}`)
+                }
+              >
+                <AvatarImage username={username} size={100} />
+                <div className="details">
+                  <div className="username">{username}</div>
+                  <div className="num-votes">{`Received ${userSubmission.NumVotes} Votes`}</div>
+                </div>
+              </WinnerCard>
+            </div>
+          );
+        }
+      } else {
+        return null;
+      }
+    }
+  }, [props.tournament, winners]);
 
   const SearchBar = (
     <div className="p-input-icon-left">
@@ -49,30 +127,18 @@ function TournamentResults(props: Props) {
       <div>
         <MixtapeBrowser LeftHeaderContent={SearchBar} mixtapes={mixtapes} />
       </div>
-      <div>
-        <div className="winner-title">Winners</div>
-        <div className="winner-desc">
-          Here are your winners! Click on them to view their winning mixtape!
-        </div>
-        {winners?.map((s, i) => (
-          <WinnerCard
-            key={i}
-            onClick={() => history.push(`/mixtape/${s.MixtapeId}`)}
-          >
-            <AvatarImage username={s.CreatedBy} size={100} />
-            <div className="details">
-              <div className="username">{s.CreatedBy}</div>
-              <div className="num-votes">{`Received ${s.NumVotes} Votes`}</div>
-            </div>
 
-            <img
-              src={`/images/medals/medal-${i + 1}.svg`}
-              height={80}
-              alt={`${i} Place`}
-              className="medal"
-            />
-          </WinnerCard>
-        ))}
+      <div>
+        {/* Tournament Winners */}
+        {yourSubmission}
+
+        <div>
+          <div className="title">Winners</div>
+          <div className="desc">
+            Here are your winners! Click on them to view their winning mixtape!
+          </div>
+          {winners.elements}
+        </div>
       </div>
     </Container>
   );
@@ -89,22 +155,35 @@ const Container = styled.div`
   grid-template-columns: 4fr 3fr;
   gap: 100px;
 
-  .winner-desc {
+  .desc {
     font-family: var(--font-secondary);
     font-size: 16px;
     color: var(--text-inactive);
   }
 
-  .winner-title {
+  .title {
     font-size: 35px;
   }
+
+  .user-submission-container {
+    margin-bottom: 20px;
+  }
 `;
+
+interface CardProps {
+  strokeColor?: string;
+}
+
+const cardStrokes = ["#EFB30C", "#7D979E", "#D88B56"];
 
 const WinnerCard = styled.div`
   display: flex;
   align-items: center;
   border-radius: 8px;
   transition: 0.2s ease-in all;
+
+  box-shadow: ${(props: CardProps) =>
+    props.strokeColor ? `inset 0px 0px 0px 1px ${props.strokeColor}` : "none"};
 
   margin-top: 15px;
   padding: 20px;
