3d876a10e7ac5c434257a80a63037c886cd669ec
sdesingh <sdesingh@pm.me>
2021-01-31 16:31:55 -0500

    :dna: added new user interface

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,26 +1,40 @@
-export class User {
-  constructor(
-    public username: string,
-    public email: string,
-    public password: string,
-    public profile: Profile = new Profile()
-  ) {}
+import { MapToObject, ObjectToMap } from "../services/util";
+
+export interface User {
+  username: string;
+  email: string;
+  pass: string;
+  profile: {
+    bio: string;
+    level: number;
+    coins: number;
+    mixtapes: Map<string, boolean>;
+    mixtapesFollowing: Map<string, boolean>;
+    followers: Map<string, boolean>;
+    following: Map<string, boolean>;
+  };
 }
 
-class Profile {
-  bio: string = "";
-  level: number = 1;
-  coins: number = 0;
+export function ParseUserFromJSON(json: any): User {
+  // Most of the types map fine.
+  const user: User = json;
 
-  favGenre = new Array<string>();
-  favArtist = new Array<string>();
-  mixtapes = new Array<string>();
+  // Maps need to converted.
+  user.profile.mixtapes = ObjectToMap(user.profile.mixtapes);
+  user.profile.mixtapesFollowing = ObjectToMap(user.profile.mixtapesFollowing);
+  user.profile.followers = ObjectToMap(json.followers);
+  user.profile.following = ObjectToMap(json.following);
 
-  totalFollowers: number = 0;
-  totalFollowing: number = 0;
-  followers = new Array<string>();
-  following = new Array<string>();
+  return user;
+}
 
-  tournamentsJoined = new Array<string>();
-  tournamentsCreated = new Array<string>();
+export function ConvertUserToJSON(user: User): any {
+  const json: any = { ...user };
+
+  json.profile.mixtapes = MapToObject(json.profile.mixtapes);
+  json.profile.mixtapesFollowing = MapToObject(json.profile.mixtapesFollowing);
+  json.profile.followers = MapToObject(json.profile.followers);
+  json.profile.following = MapToObject(json.profile.following);
+
+  return json;
 }
