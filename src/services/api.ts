5972309fb4c73a0ce0c93c8c4fbcd219cd5f4238
sdesingh <sdesingh@pm.me>
2021-02-03 17:30:20 -0500

    :broom: removed old api service

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001",
-  timeout: 8000,
-  responseType: "json",
-  validateStatus: null,
-  withCredentials: true,
-});
